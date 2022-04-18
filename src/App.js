f4298c6fb17d4e010666748a411e5588ff573f74
Bekbolsun <bekbolsun.azamatov.1579.7@gmail.com>
2022-04-18 22:59:42 +0600

    install emotion css packages

--- src/App.js
+++ src/App.js
@@ -1,39 +1,5 @@
-import styled from '@emotion/styled'
-import { css, ThemeProvider } from '@emotion/react'
-
 function App() {
-   // const state = 'red'
-   return (
-      <ThemeProvider
-         theme={css`
-            background-color: red;
-         `}
-      >
-         <div>
-            <h1>Hello</h1>
-            <Button>
-               <Span>Button</Span>
-            </Button>
-         </div>
-      </ThemeProvider>
-   )
+   return <div />
 }
-const Span = styled.div`
-   color: red;
-`
-const Button = styled.button`
-   padding: 32px;
-   background-color: ${({ props }) => props || 'blue'};
-   font-size: 24px;
-   border-radius: 4px;
-
-   font-weight: bold;
-   &:hover {
-      color: white;
-   }
-   ${Span} {
-      color: white;
-   }
-`
 
 export default App
